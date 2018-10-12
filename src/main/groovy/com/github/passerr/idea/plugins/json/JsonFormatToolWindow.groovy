599dc9f0bb4a7fe09fd465b3ad16d6bc3e56547f
PasseRR <xie__hai@sina.com>
2018-10-12 19:13:23 +0800

    add json format support

---
+++ src/main/groovy/com/github/passerr/idea/plugins/json/JsonFormatToolWindow.groovy
@@ -0,0 +1,27 @@
+package com.github.passerr.idea.plugins.json
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.content.ContentManager
+import org.jetbrains.annotations.NotNull
+
+import javax.swing.*
+
+/**
+ * json格式化窗口
+ * @author xiehai1
+ * @date 2018/10/12 15:55
+ * @Copyright ( c ) gome inc Gome Co.,LTD
+ */
+class JsonFormatToolWindow implements ToolWindowFactory {
+    @Override
+    void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        JComponent component = JsonFormatView.getInstance(project).getPanel()
+        ContentManager contentManager = toolWindow.getContentManager()
+        ContentFactory contentFactory = contentManager.getFactory()
+        contentManager.removeAllContents(true)
+        contentManager.addContent(contentFactory.createContent(component, "", true))
+    }
+}
---
+++ src/main/groovy/com/github/passerr/idea/plugins/json/JsonFormatView.groovy
@@ -0,0 +1,86 @@
+package com.github.passerr.idea.plugins.json
+
+import com.google.gson.*
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.project.Project
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants
+import org.fife.ui.rsyntaxtextarea.Theme
+import org.fife.ui.rtextarea.RTextScrollPane
+
+import javax.swing.*
+import java.awt.*
+import java.awt.event.ActionEvent
+import java.awt.event.InputEvent
+import java.awt.event.KeyEvent
+import java.util.concurrent.TimeUnit
+
+/**
+ * 格式化视图
+ * @author xiehai1
+ * @date 2018/10/12 16:02
+ * @Copyright ( c ) gome inc Gome Co.,LTD
+ */
+class JsonFormatView {
+    JPanel panel = new JPanel(new BorderLayout())
+    Project project
+    // cache instance by project
+    def private static INSTANCES = [:]
+
+    private JsonFormatView(Project project) {
+        this.project = project
+        RSyntaxTextArea textArea = new RSyntaxTextArea()
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON)
+        textArea.setCodeFoldingEnabled(true)
+        textArea.setAutoIndentEnabled(true)
+
+        textArea.getInputMap(JComponent.WHEN_FOCUSED)
+            .put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.CTRL_MASK), "format")
+        textArea.getActionMap().put("format", new AbstractAction() {
+            @Override
+            void actionPerformed(ActionEvent e) {
+                try {
+                    JsonParser jsonParser = new JsonParser()
+                    JsonObject jsonObject = jsonParser.parse(textArea.getText()).getAsJsonObject()
+                    Gson gson = new GsonBuilder().setPrettyPrinting().create()
+                    textArea.setText(gson.toJson(jsonObject))
+                } catch (JsonSyntaxException ex) {
+                    Notification n = new Notification("Json Format", "Json Format", ex.getMessage(), NotificationType.ERROR)
+                    Notifications.Bus.notify(n)
+                    new Thread() {
+                        @Override
+                        void run() {
+                            TimeUnit.SECONDS.sleep(2)
+                            n.expire()
+                        }
+                    }.start()
+                } finally {
+                    JsonFormatView.this.getPanel().repaint()
+                }
+            }
+        })
+
+        InputStream inputStream = this.getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/idea.xml")
+        try {
+            Theme theme = Theme.load(inputStream)
+            theme.apply(textArea)
+        } catch (IOException ignore) {
+        }
+        RTextScrollPane pane = new RTextScrollPane(textArea)
+        panel.add(pane)
+    }
+
+    def static getInstance(Project project) {
+        if (!INSTANCES.get(project)) {
+            synchronized (JsonFormatView.class) {
+                if (!INSTANCES.get(project)) {
+                    INSTANCES.put(project, new JsonFormatView(project))
+                }
+            }
+        }
+
+        INSTANCES.get(project) as JsonFormatView
+    }
+}
