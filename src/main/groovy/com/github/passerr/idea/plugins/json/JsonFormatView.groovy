82c0446521ee7c09abdfba740b22da3c99250c1e
PasseRR <xie__hai@sina.com>
2018-10-15 17:42:21 +0800

    bug fixed

--- src/main/groovy/com/github/passerr/idea/plugins/json/JsonFormatView.groovy
+++ src/main/groovy/com/github/passerr/idea/plugins/json/JsonFormatView.groovy
@@ -5,6 +5,7 @@ import com.intellij.notification.Notification
 import com.intellij.notification.NotificationType
 import com.intellij.notification.Notifications
 import com.intellij.openapi.project.Project
+import org.apache.commons.lang.StringUtils
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants
 import org.fife.ui.rsyntaxtextarea.Theme
@@ -20,6 +21,7 @@ import java.awt.event.KeyEvent
 import java.awt.event.MouseEvent
 import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
+
 /**
  * 格式化视图
  * @author xiehai1
@@ -45,17 +47,25 @@ class JsonFormatView {
         textArea.setCodeFoldingEnabled(true)
         textArea.setAutoIndentEnabled(true)
         textArea.getInputMap(JComponent.WHEN_FOCUSED)
+        // 使用快捷键ctrl+enter格式化json
             .put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.CTRL_MASK), "format")
         textArea.getActionMap().put("format", new AbstractAction() {
             @Override
             void actionPerformed(ActionEvent e) {
+                // 文本框为空 不进行格式化
+                if (StringUtils.isEmpty(textArea.getText())) {
+                    return
+                }
                 try {
                     textArea.setToolTipSupplier(null)
                     textArea.removeAllLineHighlights()
                     JsonParser jsonParser = new JsonParser()
-                    JsonObject jsonObject = jsonParser.parse(textArea.getText()).getAsJsonObject()
+                    // 可以同时解析数组或者Object
+                    JsonElement element = jsonParser.parse(textArea.getText())
                     Gson gson = new GsonBuilder().setPrettyPrinting().create()
-                    textArea.setText(gson.toJson(jsonObject))
+                    textArea.setText(gson.toJson(element))
+                    // 格式化成功后定位到第一行
+                    textArea.setCaretPosition(0)
                 } catch (JsonSyntaxException ex) {
                     String msg = ex.getMessage()
                     def matcher = ERROR_PATTERN.matcher(msg)
@@ -69,13 +79,15 @@ class JsonFormatView {
                             def offset = textArea.getLineOfOffset(textArea.viewToModel(me.getPoint()))
                             offset == lineIndex ? msg : null
                         } as ToolTipSupplier)
+                        // 定位到失败行
+                        textArea.setCaretPosition(lineIndex)
                     } else {
                         Notification n = new Notification("Json Format", "Json Format", msg, NotificationType.ERROR)
                         Notifications.Bus.notify(n)
                         new Thread() {
                             @Override
                             void run() {
-                                TimeUnit.SECONDS.sleep(2)
+                                TimeUnit.SECONDS.sleep(4)
                                 n.expire()
                             }
                         }.start()
