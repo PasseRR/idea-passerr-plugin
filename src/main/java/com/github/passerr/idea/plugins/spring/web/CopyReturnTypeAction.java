2c695c29c525e4249a6c2807a6de695c86b05836
PasseRR <xie__hai@sina.com>
2021-07-22 12:42:13 +0800

    添加复制返回类型为json5的动作

---
+++ src/main/java/com/github/passerr/idea/plugins/spring/web/CopyReturnTypeAction.java
@@ -0,0 +1,49 @@
+package com.github.passerr.idea.plugins.spring.web;
+
+import com.github.passerr.idea.plugins.spring.web.po.ApiDocSettingPo;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.util.ui.TextTransferable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * 复制方法返回类型为json5
+ * @author xiehai
+ * @date 2021/07/22 12:29
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class CopyReturnTypeAction extends BaseWebCopyAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        PsiMethod method = BaseWebCopyAction.method(e);
+        ApiDocSettingPo state = ApiDocStateComponent.getInstance().getState();
+        if (Objects.isNull(state)) {
+            return;
+        }
+
+        Optional.ofNullable(
+            Json5Generator.toJson5(
+                method.getReturnType(),
+                SpringWebPsiUtil.returnComment(method),
+                Collections.unmodifiableList(state.getBodyIgnoreAnnotations()),
+                Collections.unmodifiableList(state.getObjects())
+            )
+        ).ifPresent(it -> CopyPasteManager.getInstance().setContents(new TextTransferable(it)));
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        DataContext dataContext = e.getDataContext();
+        PsiElement data = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
+        // 方法上才有效
+        e.getPresentation().setEnabled(data instanceof PsiMethod);
+    }
+}
